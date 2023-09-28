110cc1207788495e0b89310f5f50cf9460e6bac9
Kostya <kostyayfek@mail.rul>
2023-09-28 15:06:00 +0500

    lession12

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+System.Console.WriteLine(numberA + numberB);
---
+++ Example008_IntroMetod/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result =arg2;
+    if(arg3 > result) result =arg3;
+    return result;
+}
+
+int a1 = 15;
+int a2 = 45;
+int a3 = 93;
+int b1 = 133568;
+int b2 = 12356;
+int b3 = 41;
+int c1 = 8578;
+int c2 = 45;
+int c3 = 99;
+
+/*int max1 = Max(a1, a2, a3);
+int max2 = Max(b1, b2, b3);
+int max3 = Max(c1, c2, c3);
+int max = Max(max1, max2, max3);*/
+
+int max = Max(Max(a1, a2, a3), Max(b1, b2, b3), Max(c1, c2, c3));
+
+Console.WriteLine(max);
